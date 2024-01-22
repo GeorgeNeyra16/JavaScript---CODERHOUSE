050db7bfbed3d0c83743ea1ba0e096c321f6e0b5
george16 <georgeneyra16@gmail.com>
2024-01-22 12:51:49 -0500

    Objetos

--- js/script.js
+++ js/script.js
@@ -10,25 +10,13 @@ nombre = prompt("Bienvenido a la plataforma de UMBRAL'98✈️, somos los especi
 
 email = prompt("Brindanos tu e-mail, así te podremos enviar el catálogo de los productos de interés👨‍✈️:")
 
-// if (edad > 0 && edad < 18){
-//     alert("Lo siento " + nombre + ", eres menor de edad,  tienes que volver con un adulto😥, Te esperamos!✈️")
-// } else if (edad === " " || edad <= 0) {
-//     alert("Lo siento " + nombre + ", indicaste una edad que no es valida😥, intentalo de nuevo!")
-//     alert(edad)
-// }else {
-//     alert("Bienvenido " + nombre + "🤗, sabemos que buscas el mejor producto personalizado pero ayudamos siendo un poco más específico, ¿Qué buscas?")
-// }
-
-
-// Bucle para pedir la edad hasta que se ingrese un número válido
-
 while (true) {
-    let consultaEdad = parseInt(prompt("Cual es tu edad " + nombre + "? 😁✌"))
-
-    if (consultaEdad !== "") {
-        if (consultaEdad >= 18){
+    edad = parseInt(prompt("Cual es tu edad " + nombre + "? 😁✌"))
+    
+    if (edad !== "") {
+        if (edad >= 18){
             alert("Bienvenido " + nombre + "🤗");
-
+            
             let consultaProducto = prompt("Sabemos que buscas el mejor producto personalizado pero ayúdanos siendo un poco más específico, ¿Qué buscas? (Indicar un número): \n1. Tazas \n2. Polos \n3. Cuadros \n4. Pines \n5. Pulseras \n6. Lanyards")
             
             while (true){
@@ -57,88 +45,20 @@ while (true) {
                             consultaProducto = prompt("Sabemos que buscas el mejor producto personalizado pero ayúdanos siendo un poco más específico, ¿Qué buscas? (Indicar un número): \n1. Tazas \n2. Polos \n3. Cuadros \n4. Pines \n5. Pulseras \n6. Lanyards")
                         }
                     }
-                break;
+                    break;
                 } else {
                     alert("No ingresaste ninguna opción😥, intentalo de nuevo!")
                     consultaProducto = prompt("Sabemos que buscas el mejor producto personalizado pero ayúdanos siendo un poco más específico, ¿Qué buscas? (Indicar un número): \n1. Tazas \n2. Polos \n3. Cuadros \n4. Pines \n5. Pulseras \n6. Lanyards")
                 }
             }
         }
-    break;
-
+        break;
+        
     } else {
         alert("Lo siento " + nombre + ", indicaste una edad que no es valida😥, intentalo de nuevo!");
-        consultaEdad = parseInt(prompt("Cual es tu edad " + nombre + "? 😁✌"));
+        edad = parseInt(prompt("Cual es tu edad " + nombre + "? 😁✌"));
     }
 }
-
-
-
-
-// while (true) {
-//     // Pregunta la edad al usuario
-//     let input = prompt("Cual es tu edad " + nombre + "? 😁✌");
-    
-
-//     // Verifica si se ingresó una edad
-//     if (input !== null) {
-//         // Convierte la entrada a un número
-//         var edad = parseInt(input);
-
-//         // Verifica si la conversión fue exitosa y la edad es un número válido
-//         if (edad > 0) {
-//             // Verifica las condiciones de ingreso
-//             if (edad >= 18) {
-//                 alert("Bienvenido " + nombre + "🤗");
-                
-//                 let consultaProducto = prompt("Sabemos que buscas el mejor producto personalizado pero ayúdanos siendo un poco más específico, ¿Qué buscas? (Indicar un número): \n1. Tazas \n2. Polos \n3. Cuadros \n4. Pines \n5. Pulseras \n6. Lanyards")
-
-//                 while (true){
-//                     if (consultaProducto !== ""){
-//                         while (true) {
-//                             if (consultaProducto === "1"){
-//                                 producto = "Tazas"
-//                                 break
-//                             } else if (consultaProducto === "2"){
-//                                 producto = "Polos"
-//                                 break
-//                             } else if (consultaProducto === "3"){
-//                                 producto = "Cuadros"
-//                                 break
-//                             } else if (consultaProducto === "4"){
-//                                 producto = "Pines"
-//                                 break
-//                             } else if (consultaProducto === "5"){
-//                                 producto = "Pulseras"
-//                                 break
-//                             } else if (consultaProducto === "6"){
-//                                 producto = "Lanyards"
-//                                 break
-//                             } else {
-//                                 alert("Ingresaste una opción que no es valida😥, intentalo de nuevo!")
-//                                 consultaProducto = prompt("Sabemos que buscas el mejor producto personalizado pero ayúdanos siendo un poco más específico, ¿Qué buscas? (Indicar un número): \n1. Tazas \n2. Polos \n3. Cuadros \n4. Pines \n5. Pulseras \n6. Lanyards")
-//                             }
-//                         }
-//                         break;
-//                     } else {
-//                         alert("No ingresaste ninguna opción😥, intentalo de nuevo!")
-//                         consultaProducto = prompt("Sabemos que buscas el mejor producto personalizado pero ayúdanos siendo un poco más específico, ¿Qué buscas? (Indicar un número): \n1. Tazas \n2. Polos \n3. Cuadros \n4. Pines \n5. Pulseras \n6. Lanyards")
-//                     }
-//                 }
-//             }
-//         } else {
-//                 alert("Lo siento " + nombre + ", eres menor de edad,  tienes que volver con un adulto😥, Te esperamos!✈️");
-//         }
-//     break; // Sale del bucle después de procesar la entrada
-//     } else if (edad < 0 || edad === 0){
-//         alert("Lo siento " + nombre + ", indicaste una edad que no es valida😥, intentalo de nuevo!");
-//         input = prompt("Cual es tu edad " + nombre + "? 😁✌");
-//     } else {
-//         alert("Lo siento " + nombre + ", no ingresaste ninguna edad😥, intentalo de nuevo!");
-//         input = prompt("Cual es tu edad " + nombre + "? 😁✌");
-//     }
-// } 
-
 
 
 let consultaMarca = prompt("Ahora por favor ayúdanos eligiendo una marca en específico (Indicar un número): \n1. BOEING \n2. AIRBUS \n3. CESSNA \n4. LAN \n5. LATAM \n6. JETSMART \n7. VIVA \n8. Sin Marca")
@@ -220,39 +140,75 @@ while (true) {
 }
 
 let precioProducto = 50;
-let delivery = 10
+let precioDelivery = 10
 
-let consultaDelivery = prompt(`Por ultimo ${nombre}. Deseas que te llevemos el producto a tu domicilio? El precio del delívery es 10 soles.? (Indicar un número): \n1. SI \n2. NO`)
+let delivery = prompt(`Por ultimo ${nombre}. Deseas que te llevemos el producto a tu domicilio? El precio del delívery es 10 soles.? (Indicar un número): \n1. SI \n2. NO`)
 
 while (true) {
-    if (consultaDelivery !== "") {
+    if (delivery !== "") {
         while(true){
 
             function servDeliv(){
-            let suma = precioProducto + delivery
+            let suma = precioProducto + precioDelivery
             return suma
             }
             let sumaFinal = servDeliv(); 
 
-            if (consultaDelivery === "1"){
+            if (delivery === "1"){
+                delivery = 1;
                 alert(`Perfecto, como resumen tenemos: \nEscogiste ${producto} como producto y que sea de la marca ${marca} y, además que sea de color ${color}. El monto final con Delivery incluído es de ${sumaFinal} soles. Te estaremos enviando el catálogo de los productos solicitados al siguiente correo: ${email}.`)
                 break
-            } else if (consultaDelivery === "2"){
+            } else if (delivery === "2"){
+                delivery = 2;
                 alert(`Perfecto, como resumen tenemos: \nEscogiste ${producto} como producto y que sea de la marca ${marca} y, además que sea de color ${color}. El monto final sin Delivery es de ${precioProducto}. Te estaremos enviando el catálogo de los productos solicitados al siguiente correo: ${email}.`)
                 break
             } else {
                 alert("Ingresaste una opción que no es valida😥, intentalo de nuevo!")
-                consultaDelivery = prompt(`Por ultimo ${nombre}. Deseas que te llevemos el producto a tu domicilio? El precio del delívery es 10 soles.? (Indicar un número): \n1. SI \n2. NO`)
+                delivery = prompt(`Por ultimo ${nombre}. Deseas que te llevemos el producto a tu domicilio? El precio del delívery es 10 soles.? (Indicar un número): \n1. SI \n2. NO`)
             }
         }
     break;
     } else {
         alert("No ingresaste ninguna opción😥, intentalo de nuevo!")
-        consultaDelivery = prompt(`Por ultimo ${nombre}. Deseas que te llevemos el producto a tu domicilio? El precio del delívery es 10 soles.? (Indicar un número): \n1. SI \n2. NO`)
+        delivery = prompt(`Por ultimo ${nombre}. Deseas que te llevemos el producto a tu domicilio? El precio del delívery es 10 soles.? (Indicar un número): \n1. SI \n2. NO`)
     }
 }
 
+//Creando un Objeto por pedido
+
+// const cliente = {
+//     name: nombre,
+//     mail: email,
+//     age: edad,
+// }
+// const pedido = {
+//     product: producto,
+//     brand: marca,
+//     color: color,
+//     delivery:delivery,
+// }
+
+//  console.log(cliente);
 
 
+//Creando un Objeto por funcion
 
- 
+function cliente(nombre, email, edad) {
+    this.nombre = nombre.toUpperCase();
+    this.email = email;
+    this.edad = edad;
+}
+
+function pedido(producto, marca, color, sumaFinal){
+    this.producto = producto;
+    this.marca = marca;
+    this.color = color;
+    this.delivery = sumaFinal
+}
+
+const cliente1 = new cliente(nombre, email,edad);
+
+const pedido1 = new pedido(producto, marca, color,servDeliv());
+
+console.log(cliente1);
+console.log(pedido1);
